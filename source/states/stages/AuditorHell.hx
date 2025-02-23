8fb1d22e89be9ff0d0f12428d7c518846d16c9a0
MatthewFro191919 <matthewfroncek4@gmail.com>
2025-02-23 08:21:49 -0500

    Update AuditorHell.hx

--- source/states/stages/AuditorHell.hx
+++ source/states/stages/AuditorHell.hx
@@ -92,12 +92,304 @@ class AuditorHell extends BaseStage
 	//    curSection
 	override function stepHit()
 	{
-		// Code here
+		super.stepHit();
+		if (FlxG.sound.music.time > Conductor.songPosition + 20 || FlxG.sound.music.time < Conductor.songPosition - 20)
+		{
+			resyncVocals();
+		}
+
+		// EX TRICKY HARD CODED EVENTS
+
+		if (curStage == 'auditorHell' && curStep != stepOfLast)
+		{
+			switch(curStep)
+			{
+				case 384:
+					doStopSign(0);
+				case 511:
+					doStopSign(2);
+					doStopSign(0);
+				case 610:
+					doStopSign(3);
+				case 720:
+					doStopSign(2);
+				case 991:
+					doStopSign(3);
+				case 1184:
+					doStopSign(2);
+				case 1218:
+					doStopSign(0);
+				case 1235:
+					doStopSign(0, true);
+				case 1200:
+					doStopSign(3);
+				case 1328:
+					doStopSign(0, true);
+					doStopSign(2);
+				case 1439:
+					doStopSign(3, true);
+				case 1567:
+					doStopSign(0);
+				case 1584:
+					doStopSign(0, true);
+				case 1600:
+					doStopSign(2);
+				case 1706:
+					doStopSign(3);
+				case 1917:
+					doStopSign(0);
+				case 1923:
+					doStopSign(0, true);
+				case 1927:
+					doStopSign(0);
+				case 1932:
+					doStopSign(0, true);
+				case 2032:
+					doStopSign(2);
+					doStopSign(0);
+				case 2036:
+					doStopSign(0, true);
+				case 2162:
+					doStopSign(2);
+					doStopSign(3);
+				case 2193:
+					doStopSign(0);
+				case 2202:
+					doStopSign(0,true);
+				case 2239:
+					doStopSign(2,true);
+				case 2258:
+					doStopSign(0, true);
+				case 2304:
+					doStopSign(0, true);
+					doStopSign(0);	
+				case 2326:
+					doStopSign(0, true);
+				case 2336:
+					doStopSign(3);
+				case 2447:
+					doStopSign(2);
+					doStopSign(0, true);
+					doStopSign(0);	
+				case 2480:
+					doStopSign(0, true);
+					doStopSign(0);	
+				case 2512:
+					doStopSign(2);
+					doStopSign(0, true);
+					doStopSign(0);
+				case 2544:
+					doStopSign(0, true);
+					doStopSign(0);	
+				case 2575:
+					doStopSign(2);
+					doStopSign(0, true);
+					doStopSign(0);
+				case 2608:
+					doStopSign(0, true);
+					doStopSign(0);	
+				case 2604:
+					doStopSign(0, true);
+				case 2655:
+					doGremlin(20,13,true);
+			}
+			stepOfLast = curStep;
+		}
 	}
+	function doGremlin(hpToTake:Int, duration:Int,persist:Bool = false)
+	{
+		interupt = false;
+
+		grabbed = true;
+		
+		totalDamageTaken = 0;
+
+		var gramlan:FlxSprite = new FlxSprite(0,0);
+
+		gramlan.frames = CachedFrames.cachedInstance.fromSparrow('grem','fourth/mech/HP GREMLIN');
+
+		gramlan.setGraphicSize(Std.int(gramlan.width * 0.76));
+
+		gramlan.cameras = [camHUD];
+
+		gramlan.x = iconP1.x;
+		gramlan.y = healthBarBG.y - 325;
+
+		gramlan.animation.addByIndices('come','HP Gremlin ANIMATION',[0,1], "", 24, false);
+		gramlan.animation.addByIndices('grab','HP Gremlin ANIMATION',[2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24], "", 24, false);
+		gramlan.animation.addByIndices('hold','HP Gremlin ANIMATION',[25,26,27,28],"",24);
+		gramlan.animation.addByIndices('release','HP Gremlin ANIMATION',[29,30,31,32,33],"",24,false);
+
+		gramlan.antialiasing = true;
+
+		add(gramlan);
+
+		if(FlxG.save.data.downscroll){
+			gramlan.flipY = true;
+			gramlan.y -= 150;
+		}
+		
+		// over use of flxtween :)
+
+		var startHealth = health;
+		var toHealth = (hpToTake / 100) * startHealth; // simple math, convert it to a percentage then get the percentage of the health
+
+		var perct = toHealth / 2 * 100;
+
+		trace('start: $startHealth\nto: $toHealth\nwhich is prect: $perct');
+
+		var onc:Bool = false;
+
+		FlxG.sound.play(Paths.sound('fourth/GremlinWoosh','clown'));
+
+		gramlan.animation.play('come');
+		new FlxTimer().start(0.14, function(tmr:FlxTimer) {
+			gramlan.animation.play('grab');
+			FlxTween.tween(gramlan,{x: iconP1.x - 140},1,{ease: FlxEase.elasticIn, onComplete: function(tween:FlxTween) {
+				trace('I got em');
+				gramlan.animation.play('hold');
+				FlxTween.tween(gramlan,{
+					x: (healthBar.x + 
+					(healthBar.width * (FlxMath.remapToRange(perct, 0, 100, 100, 0) * 0.01) 
+					- 26)) - 75}, duration,
+				{
+					onUpdate: function(tween:FlxTween) { 
+						// lerp the health so it looks pog
+						if (interupt && !onc && !persist)
+						{
+							onc = true;
+							trace('oh shit');
+							gramlan.animation.play('release');
+							gramlan.animation.finishCallback = function(pog:String) { gramlan.alpha = 0;}
+						}
+						else if (!interupt || persist)
+						{
+							var pp = FlxMath.lerp(startHealth,toHealth, tween.percent);
+							if (pp <= 0)
+								pp = 0.1;
+							health = pp;
+						}
+
+						if (shouldBeDead)
+							health = 0;
+					},
+					onComplete: function(tween:FlxTween)
+					{
+						if (interupt && !persist)
+						{
+							remove(gramlan);
+							grabbed = false;
+						}
+						else
+						{
+							trace('oh shit');
+							gramlan.animation.play('release');
+							if (persist && totalDamageTaken >= 0.7)
+								health -= totalDamageTaken; // just a simple if you take a lot of damage wtih this, you'll loose probably.
+							gramlan.animation.finishCallback = function(pog:String) { remove(gramlan);}
+							grabbed = false;
+						}
+					}
+				});
+			}});
+		});
+	}
+
+	var cloneOne:FlxSprite;
+	var cloneTwo:FlxSprite;
+
+	function doClone(side:Int)
+	{
+		switch(side)
+		{
+			case 0:
+				if (cloneOne.alpha == 1)
+					return;
+				cloneOne.x = dad.x - 20;
+				cloneOne.y = dad.y + 140;
+				cloneOne.alpha = 1;
+
+				cloneOne.animation.play('clone');
+				cloneOne.animation.finishCallback = function(pog:String) {cloneOne.alpha = 0;}
+			case 1:
+				if (cloneTwo.alpha == 1)
+					return;
+				cloneTwo.x = dad.x + 390;
+				cloneTwo.y = dad.y + 140;
+				cloneTwo.alpha = 1;
+
+				cloneTwo.animation.play('clone');
+				cloneTwo.animation.finishCallback = function(pog:String) {cloneTwo.alpha = 0;}
+		}
+
+	}
+
 	override function beatHit()
 	{
-		// Code here
+		super.beatHit();
+
+		if (generatedMusic)
+		{
+			notes.sort(FlxSort.byY, FlxSort.DESCENDING);
+		}
+
+		if (curStage == 'nevedaSpook')
+			hank.animation.play('dance');
+
+		if (SONG.notes[Math.floor(curStep / 16)] != null)
+			{
+				if (SONG.notes[Math.floor(curStep / 16)].changeBPM)
+				{
+					Conductor.changeBPM(SONG.notes[Math.floor(curStep / 16)].bpm);
+					FlxG.log.add('CHANGED BPM!');
+				}
+				// else
+				// Conductor.changeBPM(SONG.bpm);
+	
+				// Dad doesnt interupt his own notes
+				if (SONG.notes[Math.floor(curStep / 16)].mustHitSection)
+				{
+					dad.dance();
+				}
+			}
+			
+		if (curStage == 'auditorHell')
+		{
+			if (curBeat % 8 == 4 && beatOfFuck != curBeat)
+			{
+				beatOfFuck = curBeat;
+				doClone(FlxG.random.int(0,1));
+			}
+		}
+
+		iconP1.setGraphicSize(Std.int(iconP1.width + 30));
+		iconP2.setGraphicSize(Std.int(iconP2.width + 30));
+
+		iconP1.updateHitbox();
+		iconP2.updateHitbox();
+
+		if (curBeat % gfSpeed == 0 && curBeat != lastBeatT)
+		{
+			lastBeatT = curBeat;
+			gf.dance();
+		}
+
+		if (!boyfriend.animation.curAnim.name.startsWith("sing"))
+		{
+			boyfriend.playAnim('idle');
+		}
+
+		if (curBeat == 532 && curSong.toLowerCase() == "expurgation")
+		{
+			dad.playAnim('Hank', true);
+		}
+
+		if (curBeat == 536 && curSong.toLowerCase() == "expurgation")
+		{
+			dad.playAnim('idle', true);
+		}
 	}
+
 	override function sectionHit()
 	{
 		// Code here
